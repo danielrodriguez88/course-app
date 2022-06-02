7212e8a8a6c770fc9f79a47046d1adde690bfeb3
Daniel <danielorodriguez88@gmail.com>
2022-06-01 20:14:15 -0500

    initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,144 @@
+import * as model from './model.js';
+import { CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    // console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    //update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    //1.
+    await model.loadRecipe(id);
+
+    //2. Updating bookmarks
+    bookmarksView.update(model.state.bookmark);
+
+    //3. Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    //get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //load search results
+    await model.loadSearchResults(query);
+
+    //render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    //render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  //render NEW results
+  // resultsView.render(model.state.search.results);
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //update recipe servings in state
+  model.updateServings(newServings);
+
+  //update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  //Add or remove bookmarks
+  if (!model.state.recipe.bookmark) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  //update recipe view
+  recipeView.update(model.state.recipe);
+
+  //Render bookmarks to list
+  bookmarksView.render(model.state.bookmark);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmark);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    //show loading spinner
+    addRecipeView.renderSpinner();
+
+    //Upload new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    //Render recipe
+    recipeView.render(model.state.recipe);
+
+    //Success message
+    addRecipeView.renderMessage();
+
+    //Render bookmarkView
+    bookmarksView.render(model.state.bookmark);
+
+    //Change url id
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    //Clear form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error(err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipe);
+  recipeView.addHandlerServings(controlServings);
+  recipeView.addHandlerBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Welcome');
+};
+init();
